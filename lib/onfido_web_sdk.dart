fb4d66fc95e57c9340197c5d7890b2b2cb7fd666
neilhogg <6nDpEsWPqJegskZF7sG_>
2023-03-08 15:57:43 +0200

    Made some changes to the onfido package to try get interop working

--- lib/onfido_web_sdk.dart
+++ lib/onfido_web_sdk.dart
@@ -12,33 +12,29 @@ export 'src/onfido_web_sdk_base.dart';
 // Load the Onfido Web SDK JavaScript file.
 Future<void> loadSdk() async {
   final script = html.ScriptElement();
-  script.src = 'https://assets.onfido.com/web-sdk-releases/11.0.0/onfido.min.js';
+  script.src =
+  'https://assets.onfido.com/web-sdk-releases/11.0.0/onfido.min.js';
   script.async = true;
   script.defer = true;
   html.document.body?.children.add(script);
 }
 
-// Expose the Onfido Web SDK API methods as Dart methods.
-@JS()
-@anonymous
-class Onfido {
-  external factory Onfido({
-    String token,
-    String containerId,
-    Function onComplete,
-    List<String> steps});
-
-  external static void startFlow();
+void start({
+  required String token,
+  required Function onComplete,
+  required List<String> steps,
+  bool useModal = false,
+}) {
+  Onfido().init(token, useModal, onComplete, steps);
 }
 
-/*Onfido.init({
-token: '<YOUR_SDK_TOKEN>',
-containerId: 'onfido-mount',
-containerEl: <div id="root" />, //ALTERNATIVE to `containerId`
-onComplete: function (data) {
-console.log('everything is complete')
-},
-steps: ['welcome', 'poa', 'document', 'face', 'complete'],
-})*/
 
-// TODO: Export any libraries intended for clients of this package.
+@JS()
+@staticInterop
+class Onfido {
+  external factory Onfido();
+}
+
+extension on Onfido {
+  external void init(String token, bool useModal, Function onComplete, List<String> steps);
+}
