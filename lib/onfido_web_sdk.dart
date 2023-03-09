39dd6f98d2533c58483107ac97698357ac6b33a8
neilhogg <6nDpEsWPqJegskZF7sG_>
2023-03-09 13:58:15 +0200

    Made progress on calling the onfido sdk from the js package just need to get the div to show now

--- lib/onfido_web_sdk.dart
+++ lib/onfido_web_sdk.dart
@@ -5,44 +5,61 @@
 library onfido_web_sdk;
 
 import 'package:js/js.dart';
-import 'dart:html' as html;
 
 export 'src/onfido_web_sdk_base.dart';
 
 // Load the Onfido Web SDK JavaScript file.
 Future<void> loadSdk() async {
-  final script = html.ScriptElement();
-  script.src =
-  'https://assets.onfido.com/web-sdk-releases/11.0.0/onfido.min.js';
-  script.async = true;
-  script.defer = true;
-  html.document.body?.children.add(script);
+  // final script = html.ScriptElement();
+  // script.src =
+  // 'https://assets.onfido.com/web-sdk-releases/12.0.0/onfido.min.js';
+  // script.async = true;
+  // script.defer = true;
+  // html.document.body?.children.add(script);
 }
-
 
 @JS('Onfido')
 @staticInterop
 class Onfido {
-  external static void init(Map options);
+  external static dynamic init(dynamic);
 }
+
+@JS('console.log')
+external void log(dynamic str);
 
 void start({
   required String token,
   required Function onComplete,
   required List<String> steps,
-  String containerId = '',
+  String containerId = 'onfido-mount',
   bool useModal = false,
 }) {
-  var options = {
-    'token': token,
-    'containerId': containerId,
-    'steps': steps.toString(),
-    'onComplete': onComplete,
-    'useModal': useModal,
-  };
+  // log('starting onfido $options');
+  final init = allowInterop(Onfido.init);
+  init({
+    token: token,
+    steps: steps,
+    containerId: containerId,
+    onComplete: allowInterop((data) {
+      log('ON COMPLETE! $data');
+      onComplete(data);
+    }),
+    useModal: useModal,
+  });
+}
 
-  var onfidoInit = allowInterop(Onfido.init);
-  onfidoInit(options);
+class Opts {
+  final String token;
+  final List<String> steps;
+  final String containerId;
+  final Function onComplete;
+  final bool useModal;
 
-  // Onfido.init(token, '', useModal, onComplete, steps);
+  Opts({
+    required this.token,
+    required this.steps,
+    required this.onComplete,
+    this.containerId = 'onfido-mount',
+    this.useModal = false,
+  });
 }
