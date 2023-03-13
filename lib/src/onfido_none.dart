352f6135864ff6d29c4e9c12101c8791243480f6
neilhogg <6nDpEsWPqJegskZF7sG_>
2023-03-13 13:43:42 +0200

    Fixed document/country identifiers that weren't passed through to onfido

--- lib/src/onfido_none.dart
+++ lib/src/onfido_none.dart
@@ -3,7 +3,7 @@ void closeOnfido() {}
 void start({
   required String token,
   required Function onComplete,
-  required List<String> steps,
+  required List steps,
   String containerId = 'onfido-mount',
   bool useModal = false,
 }) {}
--- lib/src/onfido_web.dart
+++ lib/src/onfido_web.dart
@@ -12,7 +12,8 @@ Future<void> loadSdk() async {
   html.DivElement mount = html.DivElement(); // create a new div element
   mount.id = 'onfido-mount'; // set the ID of the new div to 'onfido-mount'
 
-  final body = html.querySelector('body') as html.BodyElement?; // select the body element of the page
+  final body = html.querySelector('body')
+      as html.BodyElement?; // select the body element of the page
   body?.append(mount);
 }
 
@@ -24,14 +25,14 @@ void closeOnfido() {
 void start({
   required String token,
   required Function onComplete,
-  required List<String> steps,
+  required List steps,
   String containerId = 'onfido-mount',
   bool useModal = false,
 }) {
   final init = allowInterop(Onfido.init);
   init(Opts(
     token: token,
-    steps: steps,
+    steps: _buildSteps(steps),
     containerId: containerId,
     useModal: useModal,
     onComplete: allowInterop((data) {
@@ -39,6 +40,19 @@ void start({
       onComplete(dartMap);
     }),
   ));
+}
+
+List _buildSteps(List steps) {
+  List resultSteps = [];
+  for (var value in steps) {
+    if (value is String) {
+      resultSteps.add(value);
+    } else if (value is Map) {
+      resultSteps.add(jsify(value));
+    }
+  }
+
+  return resultSteps;
 }
 
 @JS('Onfido')
@@ -51,14 +65,14 @@ class Onfido {
 @anonymous
 class Opts {
   external String token;
-  external List<String> steps;
+  external List steps;
   external String containerId;
   external Function(Map) onComplete;
   external bool useModal;
 
   external factory Opts({
     String token,
-    List<String> steps,
+    List steps,
     Function onComplete,
     String containerId,
     bool useModal,
